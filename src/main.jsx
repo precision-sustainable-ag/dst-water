7fb14faca0ae51d32e3dcebb50f132c0e88dbe6c
Rick Hitchcock <rickhitchcock@outlook.com>
2023-08-01 16:25:53 -0400

    cra -> vite

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import App from './App';
+import { store } from './store/Store';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+);
