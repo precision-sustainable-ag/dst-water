dc4c8558bd4bddab85ff5928160082ce0a7213b8
Rick Hitchcock <rickhitchcock@outlook.com>
2024-09-17 16:10:28 -0400

    production pipeline

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
-    // watch: {
-    //   usePolling: true,
-    // },
-    // host: true, // needed for the Docker Container port mapping to work
-    // strictPort: true,
-    // port: 3000,
   },
   optimizeDeps: {
     include: [
